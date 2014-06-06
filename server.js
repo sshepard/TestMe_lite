02d031f0af8ab1cf74c9fcb8c8ff4d12e4e73d2a
Sonoko Hirano <snkhrn@gmail.com>
2014-06-06 16:52:03 -0500

    Node.js server file
    Please run this file when executing this project

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 var express = require('express'),
-    app = express(), //bundle everything
-    server = require('http').createServer(app);
-var bodyParser = require('body-parser');
+    app = express(),
+    server = require('http').createServer(app),
+    bodyParser = require('body-parser');
 
 // parse application/json and application/x-www-form-urlencoded
-app.use(bodyParser())
+app.use(bodyParser());
 
 // parse application/vnd.api+json as json
 app.use(bodyParser.json({
@@ -22,8 +22,9 @@ app.use("/css", express.static(__dirname + '/css'));
 app.use("/js", express.static(__dirname + '/js'));
 
 app.post('/ajax', function(req, res) {
-    console.log(req.body);
+    // console.log(req.body);
     res.send(req.body);
 });
 
 server.listen(8124);
+console.log('Express server started on port %s', server.address().port);
